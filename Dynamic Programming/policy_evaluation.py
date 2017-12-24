4f048210ef0a0171782613bbf8fbf86c5399a00c
Truman Wang <truman.wang@mail.utoronto.ca>
2017-12-23 22:46:32 -0500

    created DP folder, and lib/env folder

---
+++ Dynamic Programming/policy_evaluation.py
@@ -0,0 +1,5 @@
+import numpy as numpy
+import sys
+if "../" not in sys.path:
+    sys.path.append("../")
+from lib.envs.gridworld import GridworldEnv
---
+++ lib/env/gridworld.py
@@ -0,0 +1,10 @@
+import numpy as numpy
+import sys
+from gym.envs.toy_text import discrete
+
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
+#class GridworldEnv(discrete.DiscreteEnv):
