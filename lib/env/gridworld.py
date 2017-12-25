998746aba481fdd8f51223c6c60a7d8dd92bd9a0
Truman Wang <truman.wang@mail.utoronto.ca>
2017-12-24 21:59:27 -0500

    creating gridworld

--- lib/env/gridworld.py
+++ lib/env/gridworld.py
@@ -7,4 +7,37 @@ RIGHT = 1
 DOWN = 2
 LEFT = 3
 
-#class GridworldEnv(discrete.DiscreteEnv):
+class GridworldEnv(discrete.DiscreteEnv):
+    """
+    Grid World environment from Sutton's Reinforcement Learning book chapter 4.
+    You are an agent on the MxN grid and your goal is to reach the terminal 
+    state at the top left of bottom right corner.
+
+    For example, a 4x4 grid looks as follows:
+
+    T  o  o  o
+    o  x  o  o
+    o  o  o  o
+    o  o  o  T
+
+    x is your position and T are the two terminal states.
+
+    You can take actions in each direction (UP=0, RIGHT=1, DOWN=2, LEFT=3).
+    Actions going off the edge leave you in your current state.
+    You receive a reward of -1 at each step until you reach a terminal state.
+    """
+
+    metadata = {'render.modes': ['human', 'ansi']}
+
+    def __init__(self, shape=[4,4]):
+        if not isinstance(shape, (list, tuple)) or not len(shape) == 2:
+            raise ValueError('shape argument must be a list/tuple of length 2')
+
+        self.shape = shape
+        nS = np.prod(shape)
+        nA = 4
+
+        MAX_Y = shape[0]
+        MAX_X = shape[1]
+
+        P = {}
